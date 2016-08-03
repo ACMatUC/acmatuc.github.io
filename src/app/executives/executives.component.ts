747b0e99d147a3734a642fe619144c388119ff54
Dom Farolino <domfarolino@gmail.com>
2016-08-03 17:54:32 -0400

    feat(bios): finish bios for now

--- src/app/executives/executives.component.ts
+++ src/app/executives/executives.component.ts
@@ -21,36 +21,44 @@ export class ExecutivesComponent implements OnInit {
         role: "President",
         name: "Kurt Lewis",
         image: this.imgBaseURL + "president.jpg",
-        bio: `My name is Kurt Lewis and I am a third year computer science major.
-              I am involved in student organizations such as CEAS ambassadors and engineering
-              tribunal. I've been involved with ACM for two years, previously serving as treasurer.
-              I've been programming for 5 years, and I enjoy working with new technology. I'm excited
-              about the opportunities ACM has to offer and I look forward to building a computing community
-              at UC.`
+        bio: `I'm a 3rd year computer science major involved in several student organizations
+              such as CEAS ambassadors and engineering tribunal. I've been involved with ACM for
+              two years, previously serving as treasurer. I've been programming for 5 years and
+              enjoy working with new technology. I'm excited about the opportunities ACM has to
+              offer and I look forward to building a computing community at UC.`
       },
       {
         role: "Vice President",
         name: "Juanita Dickhaus",
         image: this.imgBaseURL + "vice-president.jpg",
-        bio: `Apparently this executive likes to keep an air of mystery about themselves...`
+        bio: `I'm a 5th year computer science major involved in organizing hackathons as well as several
+              student organizations including ACM-W. I do mostly JavaScript and front-end development on co-op,
+              and am working towards a stronger level of fluency in general full stack development. I'm currently
+              interested in data science and engineering research. Feel free to contact me at juanitadickhaus@gmail.com
+              if you'd like academic help, advice, or to connect with various student groups on campus.`
       },
       {
         role: "Treasurer",
         name: "Kyle Arens",
         image: this.imgBaseURL + "treasurer.jpg",
-        bio: `Apparently this executive likes to keep an air of mystery about themselves...`
+        bio: `I’m a 4th year computer science student here at UC. In addition to serving as the treasurer for ACM, I’m
+              also the Vice President for the UC Club Ice Hockey Team. I’m looking forward to helping run ACM this year, and
+              bring great events and a great community to our members.`
       },
       {
         role: "Secretary",
         name: "Noah Bass",
         image: this.imgBaseURL + "secretary.jpg",
-        bio: `Apparently this executive likes to keep an air of mystery about themselves...`
+        bio: `I'm a 2nd year computer science major at UC, and my favorite language is JavaScript. My spirit animal is a turtle,
+              my favorite color is blue, and I'm currently interning with Siemens PLM Software.`
       },
       {
         role: "Web Master",
         name: "Dom Farolino",
         image: this.imgBaseURL + "webmaster.jpg",
-        bio: `Apparently this executive likes to keep an air of mystery about themselves...`
+        bio: `I'm a 3rd year computer science major at UC. In my free time I like to study algorithms and math as well as build
+              progressive web apps. My favorite languages are C++ and JavaScript in that order. I'm a big advocate of the modern
+              open web platform and wish to work somewhere that allows me to make the web do everything native can on every platform.`
       }
     ];
   }
