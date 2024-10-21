9a3a120f0cc826e95a2f5017367e6668cbc62d9b
Daniel Budke <40306561+dabudke@users.noreply.github.com>
2024-10-21 13:25:22 -0400

    feat: rework navbar using tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,12 +15,18 @@ export default {
 					'400': '#ff7469',
 					'500': '#f9483a',
 					'600': '#e83224',
+					DEFAULT: "#e83224",
 					'700': '#c31f12',
 					'800': '#a11d13',
 					'900': '#851f17',
 					'950': '#490b06',
 				},
+				instagram: "#ff0069",
+				discord: "#5865f2",
+				linkedin: "#0072b1",
 			},
+			transitionTimingFunction: "linear",
+			transitionDuration: "100ms"
 		},
 		fontFamily: {
 			sans: ['Open Sans', 'sans-serif'],
