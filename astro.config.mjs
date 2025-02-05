862da617068b102d96d9c988ae9138012ccdadc9
Daniel Budke <dbudke1010@outlook.com>
2025-02-05 10:49:11 -0500

    bump: upgrade astro to v5.2.3
    bump tailwind to v4.0.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@tailwindcss/vite';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwind()],
+  }
 });
