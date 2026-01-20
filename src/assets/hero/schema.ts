3165ad8e24682385fe528c70df4367e8fb461937
Daniel Budke <dbudke1010@outlook.com>
2026-01-20 15:41:53 -0500

    feat: use image for hero
    eventually replace this with a rotating set of hero images

--- src/assets/hero/schema.ts
+++ src/assets/hero/schema.ts
@@ -5,17 +5,19 @@ import { z } from "astro:schema";
 // index of 0 marks the base image (e.g. ACM@UC banner)
 // typically 1080x1920, portrait and landscape
 
+// TODO - multiple hero images for announcements/upcoming events
+
 const heroImages = defineCollection({
   loader: glob({
     base: "./src/assets/hero",
     pattern: "*/*.(yml|yaml)",
+    generateId: ({ entry }) => entry.split("/")[0],
   }),
   schema: ({ image }) =>
     z.object({
       landscape: image(),
       portrait: image(),
       alt: z.string(),
-      slug: z.optional(z.string()),
       index: z.number(),
     }),
 });
