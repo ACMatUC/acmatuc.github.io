ca930147b88184702240d2bf8501f64d8f8ad49b
Daniel Budke <dbudke1010@outlook.com>
2026-01-20 19:08:00 -0500

    feat: move executives to data folder
    also start converting to 2025-2026 board, better late than never i guess :shrug:

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,10 +2,12 @@ import blog from "./blog/schema";
 import members from "./members/schema";
 import meetings from "./meetings/schema";
 import heroImages from "$assets/hero/schema";
+import executiveBoard from "data/executives.schema";
 
 export const collections = {
   blog,
   members,
   meetings,
   heroImages,
+  executiveBoard,
 };
